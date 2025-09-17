04e67aa41ed33439ad142f47187d5e2b2ac19717
megumin123578 <saranghae123578@gmail.com>
2025-09-17 11:56:53 +0700

    update ligt/dark mode feature, fix card border

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,4 +1,6 @@
-import {  Card, CardHeader, CardTitle, CardDescription } from "@/components/ui/card"
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { LogInIcon } from "lucide-react";
 
 export default function LoginPage(){
     return(
@@ -7,7 +9,16 @@ export default function LoginPage(){
                 <CardTitle className="text-xl">Welcome Back!</CardTitle>
                 <CardDescription>Login with your email</CardDescription>
             </CardHeader>
+            <CardContent>
+                <Button className="bg-neutral-900 text-white hover:bg-green-600 focus-visible:ring-2 focus-visible:ring-green-600 w-full" variant = "outline">
+                    <LogInIcon className="size-4"/>
+                    Sign in with Google account
+                </Button>
+
+
+            </CardContent>
         </Card>
+
     );
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { ThemeToggle } from "@/components/ui/themeToggle";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Hello</div>
+    <div>Hello
+      <ThemeToggle/>
+    </div>
+    
   );
 }
